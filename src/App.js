96f5797a57359d12b0ae5d1ca5a909be13eed864
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-09-28 16:43:25 +0200

    added images, updated index.html, setup basic scss for styling, setup components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.scss';
+import "./App.scss";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <SearchBar />
+      <Content />
     </div>
   );
 }
