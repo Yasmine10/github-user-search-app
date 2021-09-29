89d3394fcf8f34d371f067155ec03e4252851dcd
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-09-29 17:13:59 +0200

    changed initial value darkMode

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Content from "./components/Content";
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   const toggleDarkMode = () => setDarkMode(!darkMode);
 
   console.log(darkMode);
